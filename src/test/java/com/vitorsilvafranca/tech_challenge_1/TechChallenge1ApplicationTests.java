a2a5f2fb936d2e4ee1721c3cc8a4c905c6494f76
vitor-silva-franca <sf_vitor@outlook.com>
2025-05-18 12:50:56 -0300

    .

---
+++ src/test/java/com/vitorsilvafranca/tech_challenge_1/TechChallenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorsilvafranca.tech_challenge_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
